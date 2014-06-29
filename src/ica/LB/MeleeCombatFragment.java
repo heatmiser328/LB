415e64305d02feacb37d6166633672c00f41d5a1
Jeff Capuano <jcapuano328@gmail.com>
2014-06-28 20:19:39 -0400

    melee calc dialog

--- src/ica/LB/MeleeCombatFragment.java
+++ src/ica/LB/MeleeCombatFragment.java
@@ -1,8 +1,8 @@
 package ica.LB;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 //import android.app.Fragment;
-import android.content.*;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.LayoutInflater;
@@ -15,6 +15,7 @@ import android.widget.*;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+import ica.LB.MeleeCalcDialog.OnMeleeCalcFinishListener;
 import ica.LB.Core.*;
 import ica.LB.Helpers.*;
 
@@ -50,32 +51,6 @@ public class MeleeCombatFragment extends Fragment {
 	private TextView txtMeleeLeaderLoss;
 	private ImageView imgMeleeLeaderLossSide;
 	private ImageView imgMeleeLeaderLoss;
-	
-	private Button btnMeleeIncrPrev;
-	private Button btnMeleeIncrNext;
-	private EditText editMeleeIncrValue;
-	
-	private Button btnMeleeLossPrev;
-	private Button btnMeleeLossNext;
-	private EditText editMeleeLossValue;
-	
-	private Button btnMeleeValuePrev;
-	private Button btnMeleeValueNext;
-	private EditText editMeleeValueValue;
-	
-	private Button btnMeleeLancePrev;
-	private Button btnMeleeLanceNext;
-	private EditText editMeleeLanceValue;
-	
-	private Button btnMeleeTotalPrev;
-	private Button btnMeleeTotalNext;
-	private EditText editMeleeTotalValue;
-	
-	private ToggleButton btnMeleeMods13;
-	private ToggleButton btnMeleeMods12;
-	private ToggleButton btnMeleeMods32;
-	private ToggleButton btnMeleeMods2;
-	private ToggleButton btnMeleeModsLnc;
 	
 	private Dice dice;
 	private MeleeCombat mc;
@@ -124,40 +99,8 @@ public class MeleeCombatFragment extends Fragment {
 		    txtMeleeLeaderLoss = (TextView)rootView.findViewById(R.id.txtMeleeLeaderLoss);
 		    imgMeleeLeaderLoss = (ImageView)rootView.findViewById(R.id.imgMeleeLeaderLoss);
         
-            // calc
-		    btnMeleeIncrPrev	 = (Button)rootView.findViewById(R.id.btnMeleeIncrPrev	);
-		    btnMeleeIncrNext	 = (Button)rootView.findViewById(R.id.btnMeleeIncrNext	);
-		    editMeleeIncrValue	 = (EditText)rootView.findViewById(R.id.editMeleeIncrValue);
-							
-		    btnMeleeLossPrev	 = (Button)rootView.findViewById(R.id.btnMeleeLossPrev	);
-		    btnMeleeLossNext	 = (Button)rootView.findViewById(R.id.btnMeleeLossNext	);
-		    editMeleeLossValue	 = (EditText)rootView.findViewById(R.id.editMeleeLossValue);
-		
-		    btnMeleeValuePrev	 = (Button)rootView.findViewById(R.id.btnMeleeValuePrev	);
-		    btnMeleeValueNext	 = (Button)rootView.findViewById(R.id.btnMeleeValueNext	);
-		    editMeleeValueValue	 = (EditText)rootView.findViewById(R.id.editMeleeValueValue);
-		
-		    btnMeleeLancePrev	 = (Button)rootView.findViewById(R.id.btnMeleeLancePrev	);
-		    btnMeleeLanceNext	 = (Button)rootView.findViewById(R.id.btnMeleeLanceNext	);
-		    editMeleeLanceValue	 = (EditText)rootView.findViewById(R.id.editMeleeLanceValue);
-		
-		    btnMeleeTotalPrev	 = (Button)rootView.findViewById(R.id.btnMeleeTotalPrev	);
-		    btnMeleeTotalNext	 = (Button)rootView.findViewById(R.id.btnMeleeTotalNext	);
-		    editMeleeTotalValue	 = (EditText)rootView.findViewById(R.id.editMeleeTotalValue);
-		
-		    btnMeleeMods13  = (ToggleButton)rootView.findViewById(R.id.btnMeleeMods13 );
-		    btnMeleeMods12  = (ToggleButton)rootView.findViewById(R.id.btnMeleeMods12 );
-		    btnMeleeMods32  = (ToggleButton)rootView.findViewById(R.id.btnMeleeMods32 );
-		    btnMeleeMods2   = (ToggleButton)rootView.findViewById(R.id.btnMeleeMods2  );
-		    btnMeleeModsLnc = (ToggleButton)rootView.findViewById(R.id.btnMeleeModsLnc);
-		
-		    editMeleeAttackerValue.setText("1");
-		    editMeleeDefenderValue.setText("1");
-		    editMeleeIncrValue.setText("1");
-		    editMeleeLossValue.setText("1");
-		    editMeleeValueValue.setText("1");
-		    editMeleeLanceValue.setText("1");
-		    editMeleeTotalValue.setText("1");
+		    editMeleeAttackerValue.setText("0");
+		    editMeleeDefenderValue.setText("0");
         
         
 		    // attacker
@@ -196,11 +139,20 @@ public class MeleeCombatFragment extends Fragment {
 		    btnMeleeAttackerAdd.setOnClickListener(new OnClickListener() {
 			    @Override
 			    public void onClick(View arg0) {
-			        double value = getAttackerValue();
-                    value += getMeleeTotal();
-			        editMeleeAttackerValue.setText(Double.toString(value));
-			        calcOdds();
-			        updateResults();
+                    FragmentManager fm = getActivity().getSupportFragmentManager();
+                    MeleeCalcDialog dlg = new MeleeCalcDialog();
+                    dlg.setFinishListener(new OnMeleeCalcFinishListener() {
+                        @Override
+                        public void onFinishDialog(Double v) {
+                            // add the value to the appropriate total
+			                double value = getAttackerValue();
+                            value += v;
+			                editMeleeAttackerValue.setText(Double.toString(value));
+			                calcOdds();
+			                updateResults();
+                        }
+                    });
+                    dlg.show(fm, "dlg_melee_calc");
 			    }
 		    });
             
@@ -249,11 +201,20 @@ public class MeleeCombatFragment extends Fragment {
 		    btnMeleeDefenderAdd.setOnClickListener(new OnClickListener() {
 			    @Override
 			    public void onClick(View arg0) {
-			        double value = getDefenderValue();
-                    value += getMeleeTotal();
-			        editMeleeDefenderValue.setText(Double.toString(value));
-			        calcOdds();
-			        updateResults();
+                    FragmentManager fm = getActivity().getSupportFragmentManager();
+                    MeleeCalcDialog dlg = new MeleeCalcDialog();
+                    dlg.setFinishListener(new OnMeleeCalcFinishListener() {
+                        @Override
+                        public void onFinishDialog(Double v) {
+                            // add the value to the appropriate total
+			                double value = getDefenderValue();
+                            value += v;
+			                editMeleeDefenderValue.setText(Double.toString(value));
+			                calcOdds();
+			                updateResults();
+                        }
+                    });
+                    dlg.show(fm, "dlg_melee_calc");
 			    }
 		    });
             
@@ -332,216 +293,18 @@ public class MeleeCombatFragment extends Fragment {
 		    });
             
 		
-		    btnMeleeIncrPrev.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeIncrements();
-			        if (--value < 1) value = 1;
-			        editMeleeIncrValue.setText(Integer.toString(value));
-                    updateUnit();
-			    }
-		    });
-		    btnMeleeIncrNext.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeIncrements();
-			        editMeleeIncrValue.setText(Integer.toString(++value));
-                    updateUnit();
-			    }
-		    });
-		    editMeleeIncrValue.addTextChangedListener(new TextWatcher() {
-			    @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-			    @Override
-                public void onTextChanged(CharSequence s, int start, int before, int end) {
-                }
-                public void afterTextChanged(Editable s) {
-			        updateUnit();
-                }
-            });
-		
-		    btnMeleeLossPrev.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeLoss();
-			        if (--value < 1) value = 1;
-			        editMeleeLossValue.setText(Integer.toString(value));
-                    updateUnit();
-			    }
-		    });
-		    btnMeleeLossNext.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeLoss();
-			        editMeleeLossValue.setText(Integer.toString(++value));
-                    updateUnit();
-			    }
-		    });
-		    editMeleeLossValue.addTextChangedListener(new TextWatcher() {
-			    @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-			    @Override
-                public void onTextChanged(CharSequence s, int start, int before, int end) {
-                }
-                public void afterTextChanged(Editable s) {
-			        updateUnit();
-                }
-            });
-		
-		    btnMeleeValuePrev.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeValue();
-			        if (--value < 1) value = 1;
-			        editMeleeValueValue.setText(Integer.toString(value));
-                    updateUnit();
-			    }
-		    });
-		    btnMeleeValueNext.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeValue();
-			        editMeleeValueValue.setText(Integer.toString(++value));
-                    updateUnit();
-			    }
-		    });
-		    editMeleeValueValue.addTextChangedListener(new TextWatcher() {
-			    @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-			    @Override
-                public void onTextChanged(CharSequence s, int start, int before, int end) {
-                }
-                public void afterTextChanged(Editable s) {
-			        updateUnit();
-                }
-            });
-		
-		    btnMeleeLancePrev.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeLance();
-			        if (--value < 1) value = 1;
-			        editMeleeLanceValue.setText(Integer.toString(value));
-                    updateUnit();
-			    }
-		    });
-		    btnMeleeLanceNext.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        int value = getMeleeLance();
-			        editMeleeLanceValue.setText(Integer.toString(++value));
-                    updateUnit();
-			    }
-		    });
-		    editMeleeLanceValue.addTextChangedListener(new TextWatcher() {
-			    @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-			    @Override
-                public void onTextChanged(CharSequence s, int start, int before, int end) {
-                }
-                public void afterTextChanged(Editable s) {
-			        updateUnit();
-                }
-            });
-		
-		    btnMeleeTotalPrev.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        double value = getMeleeTotal();
-			        if (--value < 1) value = 1;
-			        editMeleeTotalValue.setText(Double.toString(value));
-			    }
-		    });
-		    btnMeleeTotalNext.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        double value = getMeleeTotal();
-			        editMeleeTotalValue.setText(Double.toString(++value));
-			    }
-		    });
-            /*
-		    editMeleeTotalValue.addTextChangedListener(new TextWatcher() {
-			    @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-			    @Override
-                public void onTextChanged(CharSequence s, int start, int before, int end) {
-                }
-                public void afterTextChanged(Editable s) {
-                }
-            });
-		    */
-		
-		    btnMeleeMods13.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        updateUnit();
-			    }
-		    });
-		    btnMeleeMods12.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        updateUnit();
-			    }
-		    });
-		    btnMeleeMods32.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        updateUnit();
-			    }
-		    });
-		    btnMeleeMods2.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        updateUnit();
-			    }
-		    });
-		
-		    btnMeleeModsLnc.setOnClickListener(new OnClickListener() {
-			    @Override
-			    public void onClick(View arg0) {
-			        updateUnit();
-			    }
-		    });
-
 		    displayOdds();
 		    displayDice();
         }   
 
         return rootView;
     }
-    
+ 
 	void calcOdds() {
 		odds = mc.calculate(getAttackerValue(), getDefenderValue());
 		if (odds == null)
 			odds = mc.getDefaultOdds();
 		displayOdds();
-	}
-	
-	void updateUnit() {
-        int incr = getMeleeIncrements();
-	    int loss = getMeleeLoss();
-		double melee = getMeleeValue();
-		double lance = getMeleeLance();
-        
-		double val = melee * ((double)(incr - loss) / (double)incr);
-		if (btnMeleeMods13.isChecked()) 
-	        val /= 3.0;
-		if (btnMeleeMods12.isChecked()) 
-	        val /= 2.0;
-		if (btnMeleeMods32.isChecked()) 
-	        val *= 1.5;
-		if (btnMeleeMods2.isChecked()) 
-	        val *= 2.0;
-        
-		if (btnMeleeModsLnc.isChecked()) 
-            lance *= 2.0;
-        
-		editMeleeTotalValue.setText(Double.toString(val + lance));
 	}
 	
 	void updateResults() {
@@ -580,36 +343,6 @@ public class MeleeCombatFragment extends Fragment {
 	}
 	double getDefenderValue() {
         String v = editMeleeDefenderValue.getText().toString();
-        if (!v.isEmpty())
-            return Double.parseDouble(v);
-        return 1;           
-	}
-	int getMeleeIncrements() {
-        String v = editMeleeIncrValue.getText().toString();
-        if (!v.isEmpty())
-            return Integer.parseInt(v);
-        return 1;
-	}
-	int getMeleeLoss() {
-        String v = editMeleeLossValue.getText().toString();
-        if (!v.isEmpty())
-            return Integer.parseInt(v);
-        return 1;
-	}
-	int getMeleeValue() {
-        String v = editMeleeValueValue.getText().toString();
-        if (!v.isEmpty())
-            return Integer.parseInt(v);
-        return 1;
-	}
-	int getMeleeLance() {
-        String v = editMeleeLanceValue.getText().toString();
-        if (!v.isEmpty())
-            return Integer.parseInt(v);
-        return 1;
-	}
-	double getMeleeTotal() {
-        String v = editMeleeTotalValue.getText().toString();
         if (!v.isEmpty())
             return Double.parseDouble(v);
         return 1;           
